006491a0a5a709c3eabad2dc409f8d3f995844f3
MatthiasDT93 <matthiasdutre@msn.com>
2025-07-23 14:54:48 +0200

    added course repository

---
+++ HorsesForCourses.WebApi/InMemoryCourseRepository.cs
@@ -0,0 +1,24 @@
+using HorsesForCourses.Core;
+
+public class InMemoryCourseRepository
+{
+    private readonly List<Course> _courses = new();
+
+    public void SaveCourse(Course course)
+    {
+        int index = _courses.FindIndex(c => c.CourseName == course.CourseName);
+        if (index >= 0)
+        {
+            _courses[index] = course;
+        }
+        else
+        {
+            _courses.Add(course);
+        }
+    }
+
+    public Course? GetByCourseName(string name)
+    {
+        return _courses.FirstOrDefault(c => c.CourseName == name);
+    }
+}
