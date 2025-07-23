608d40ce1e8f8edd852fca7d6067581fd1df5e57
MatthiasDT93 <matthiasdutre@msn.com>
2025-07-23 11:19:33 +0200

    editing program.cs to include exceptionhandler

--- HorsesForCourses.WebApi/Program.cs
+++ HorsesForCourses.WebApi/Program.cs
@@ -1,3 +1,10 @@
+using Serilog;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using HorsesForCourses.WebApi.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -19,6 +33,24 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/problem+json";
+
+        var problem = new ProblemDetails
+        {
+            Title = "Er is een fout opgetreden",
+            Status = 500,
+            Detail = "Er ging iets mis tijdens het verwerken van je verzoek."
+        };
+
+        await context.Response.WriteAsJsonAsync(problem);
+    });
+});
 
 app.MapControllers();
 
