0aef9b2a48ee1a6481dc2872039aa6a6a00af99d
MatthiasDT93 <matthiasdutre@msn.com>
2025-07-23 10:08:01 +0200

    creating customwebappfactory and adding serilog package

---
+++ HorsesForCourses.Tests/CustomWebAppFactory.cs
@@ -0,0 +1,7 @@
+// using System.IO;
+// using Microsoft.AspNetCore.Mvc.Testing;
+// using Microsoft.Extensions.Hosting;
+
+// namespace HorsesForCourses.Tests;
+
+// public class CustomWebAppFactory : WebApplicationFactory<Program> { }
