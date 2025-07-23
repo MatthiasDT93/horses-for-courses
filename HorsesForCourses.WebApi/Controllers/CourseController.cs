36fa9a3415bf4b51bfc838303df814897e11113a
MatthiasDT93 <matthiasdutre@msn.com>
2025-07-23 15:00:01 +0200

    changes in course controller

--- HorsesForCourses.WebApi/Controllers/CourseController.cs
+++ HorsesForCourses.WebApi/Controllers/CourseController.cs
@@ -1,3 +1,4 @@
+using HorsesForCourses.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HorsesForCourses.WebApi.Controllers;
@@ -8,12 +9,21 @@ public class CourseController : ControllerBase
 {
 
     private readonly ILogger<CourseController> _logger;
+    private readonly InMemoryCourseRepository _repository;
 
-    public CourseController(ILogger<CourseController> logger)
+    public CourseController(ILogger<CourseController> logger, InMemoryCourseRepository repository)
     {
         _logger = logger;
+        _repository = repository;
     }
 
-
+    [HttpPost("/courses")]
+    public ActionResult<Course> AddCoach([FromBody] Course courserequest)
+    {
+        //var mail = EmailAddress.From(coachrequest.Email);
+        var course = new Course(courserequest.CourseName, courserequest.StartDate, courserequest.EndDate);
+        _repository.SaveCourse(course);
+        return Ok();
+    }
 
 }
